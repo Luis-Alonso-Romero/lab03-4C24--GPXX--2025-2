abddd410d620fc8eb5f0ad9ce9826614eeaad681
soporte <soporte@LAB02-PC17>
2025-09-01 17:30:23 -0500

    Merge branch 'sprint-1' of https://github.com/Luis-Alonso-Romero/lab03-4C24--GPXX--2025-2.git into sprint-2

--- src/main/java/com/tecsup/lab03/controller/App.java
+++ src/main/java/com/tecsup/lab03/controller/App.java
@@ -2,6 +2,6 @@ package com.tecsup.lab03.controller;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.out.println("HOLA UNIVERSO");
   }
 }
